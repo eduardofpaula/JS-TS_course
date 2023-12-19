4468ea381478cbef84377013bcc71bcf268284cc
Eduardo Farias de Paula <eduardo.paula01@fatec.sp.gov.br>
2023-12-19 01:55:58 -0300

    html inicial and variables

---
+++ basic_js/variaveis.js
@@ -0,0 +1,9 @@
+// Não criar variavel com palavras da linguagem
+// Variaveis com nomes significavtivos que ajudam no entendimento
+// Nome da variavel não pode começar com numero
+// Não podem conter espaços ou traços
+// Utilizar camelCase
+// Case-sensitive (Letras maiusculas e minusculas fazem diferença)
+// Não redeclarar variavel com let
+// Não utilizar VAR, utilizar LET ou CONST
+let nome = "Edu";
---
+++ js/index.js
@@ -0,0 +1,3 @@
+console.log('Olá Mundo!')
+console.log('Este console sera exibido no terminal do navegador')
+alert('Ola mundo!')
