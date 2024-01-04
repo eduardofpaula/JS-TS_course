44501ccbe4979e69f38654f25efd953442046a8d
Eduardo Farias de Paula <eduardo.paula01@fatec.sp.gov.br>
2024-01-04 00:22:00 -0300

    continuando OpArit

--- basic_js/arithmeticOperators.js
+++ basic_js/arithmeticOperators.js
@@ -27,6 +27,30 @@ const epa1 = 8211;
 const epa2 = 999;
 console.log("multiplicação:",(epa1 * epa2)* epa2);
 
+// contador
+// incremento ++
+// decremento --
+let contador = 1;
+contador++; //2
+++contador; //3
+++contador; //4
+++contador; //5
+--contador; //4
+--contador; //3
+console.log(contador);
+
+let contagem = 2 
+contagem += 2; 
+contagem += 2; 
+contagem  = 2; 
+contagem *= 2; 
+console.log(contagem);
+
+// error NaN - Not a Number
+const edu2 = 10;
+const edu1 = parseInt('10');
+console.log(edu2 + edu1);
+console.log(typeof edu1);
 
 
 
